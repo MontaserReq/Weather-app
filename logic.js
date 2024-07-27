b1a888f136d74ef12ace1bf0bb99b7867b409e91
MontaserReq <MontaserReq@gmail.com>
2024-07-27 14:44:12 +0300

    add some changes

--- logic.js
+++ logic.js
@@ -19,22 +19,22 @@ async function checkWeather(city) {
 
     //control images
     if (data.weather[0].main == "Clouds") {
-        image.src = "/images/clouds.png";
+        image.src = "images/clouds.png";
     }
     if (data.weather[0].main == "Clear") {
-        image.src = "/images/clear.png";
+        image.src = "images/clear.png";
     }
     if (data.weather[0].main == "Drizzle") {
-        image.src = "/images/drizzle.png";
+        image.src = "images/drizzle.png";
     }
     if (data.weather[0].main == "Rain") {
-        image.src = "/images/rain.png";
+        image.src = "images/rain.png";
     }
     if (data.weather[0].main == "Mist") {
-        image.src = "/images/mist.png";
+        image.src = "images/mist.png";
     }
     if (data.weather[0].main == "Snow") {
-        image.src = "/images/snow.png";
+        image.src = "images/snow.png";
     }
 }
 
